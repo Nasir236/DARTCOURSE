2dc84564ca8cc302c05ea66086a6b7acf54bf859
NasirHussain <nasirzaib8587@gmail.com>
2024-12-06 22:02:13 -0800

    nasir

--- listsort.dart
+++ listsort.dart
@@ -1,7 +1,7 @@
 void main() {
   var mylist = [2, 3, 5, 6, 8, 9, 7, 1, 11, 17, 12, 10];
   print(mylist);
-  print("LIst Sorting");
+  print("List Sorting");
   // List to Sort
   mylist.sort();
   print(mylist);
