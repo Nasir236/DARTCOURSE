8f35a36efdb9149267e22412fec0d79391c6aab4
NasirHussain <nasirzaib8587@gmail.com>
2024-12-12 02:05:30 -0800

    removeWhere

--- removewhere.dart
+++ removewhere.dart
@@ -1,8 +1,8 @@
 void main() {
   // remove Where function use
-  var myList = [12, 22, 11, 55, 65, 64, 32, 56, 32];
+  var myList = [12, 1, 3, 4, 5, 6, 7, 8, 22, 11, 55, 65, 64, 32, 56, 32];
   print(myList);
   // remove where
-  myList.removeWhere((i) => (i) < 3);
+  myList.removeWhere((i) => (i) < 8);
   print(myList);
 }
