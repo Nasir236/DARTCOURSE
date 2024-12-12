abb481d1ec7c8a671ae085c29754ba9aa93a4702
NasirHussain <nasirzaib8587@gmail.com>
2024-12-12 05:59:00 -0800

    Map

---
+++ map.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, dynamic> worlds = {
+    'Country': 'Pakistan',
+    'Language': 'Urdu',
+    'Currency': 'PKR',
+    'Capital': 'Islambad'
+  };
+
+  print(worlds);
+  print(worlds.length);
+  print(worlds.keys);
+  print(worlds.values);
+}
