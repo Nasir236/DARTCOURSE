dde14b939d570ccc685586ac7d62f2537a7921ae
NasirHussain <nasirzaib8587@gmail.com>
2024-12-12 06:06:49 -0800

    map update

--- map.dart
+++ map.dart
@@ -10,4 +10,12 @@ void main() {
   print(worlds.length);
   print(worlds.keys);
   print(worlds.values);
+
+  var name = {
+    '1': 'Khan',
+    '2': 'Zakir',
+    '3': 'Nasir',
+    '4': 'Shifa',
+  };
+  print(name);
 }
