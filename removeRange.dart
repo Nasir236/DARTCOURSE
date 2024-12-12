7ba94ac85e0136c11221778ef8426c9c6b20b2a4
NasirHussain <nasirzaib8587@gmail.com>
2024-12-12 01:50:47 -0800

    removeRange

---
+++ removeRange.dart
@@ -0,0 +1,23 @@
+void main() {
+  var myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  print(myList);
+  // removeRange
+  myList.removeRange(5, 11);
+  print(myList);
+
+  // string
+  var MyList = [
+    'Nasir',
+    'Zakir',
+    'Bilal',
+    'Wasim',
+    'Khiyam',
+    'Saddam',
+    'Hilal'
+  ];
+  print(MyList);
+
+  // removeRange
+  MyList.removeRange(2, 6);
+  print(MyList);
+}
