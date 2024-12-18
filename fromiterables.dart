76cd053460e20aa17d88ff7adaea3bc5da89fb89
NasirHussain <nasirzaib8587@gmail.com>
2024-12-18 07:27:01 -0800

    dartcourse

---
+++ fromiterables.dart
@@ -0,0 +1,18 @@
+void main() {
+  var romanCount = [
+    'I',
+    "III",
+    "III",
+    "IV",
+    "V",
+    "VI",
+    "VII",
+    "VIII",
+    "IX",
+    "X"
+  ];
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  var data = Map.fromIterables(romanCount, numbers);
+  print(data);
+}
